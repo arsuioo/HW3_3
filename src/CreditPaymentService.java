6adc56ee5ecadf9579e41ca1107767a8862bbd5c
Arsh_174 <74has@mail.ru>
2024-02-03 15:41:16 +0500

    reformateCode

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
-        public double calculate(int time, int sum, double procent) {
-            int month = time * 12; // кол-во платежей
-            double stavka = procent/100/12;//ежемесячная процентная ставка
-            double anuent = (stavka+(stavka)/(Math.pow(1+stavka, month)-1));
-            double result = sum * anuent;
-            return result;
-        }
+    public double calculate(int time, int sum, double procent) {
+        int month = time * 12; // кол-во платежей
+        double stavka = procent / 100 / 12;//ежемесячная процентная ставка
+        double anuent = (stavka + (stavka) / (Math.pow(1 + stavka, month) - 1));
+        double result = sum * anuent;
+        return result;
     }
+}
 
